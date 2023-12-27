39c4aba600212d3474e50e73e86bcfc3f20e98c1
kissghost <kissghost@NB-A58Kh>
2023-12-27 23:56:42 +0900

    [2023.12.27] Mojito Nexacro Grid & Dataset 유용한 Method 구현

--- src/main/webapp/nexacroN/_extlib_/Mojito/Mojito.js
+++ src/main/webapp/nexacroN/_extlib_/Mojito/Mojito.js
@@ -4,21 +4,23 @@
 // 2023.12.10 최초작성 v1.0
 
 // Class 생성자
-nexacro._Mojito = function(_a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k, _l) {
+nexacro.Mojito = function(_a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k, _l) {
 	//nexacro.Component.call(this, _a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k, _l);
 	this._config = { isDebug: true };
 	
-	// ...
+	// ... 
 };
-var pMojito = nexacro._Mojito.prototype = nexacro._createPrototype(nexacro.Object, nexacro._Mojito);  // Prototype Getter
+nexacro.mojito = nexacro._createPrototype(nexacro.Object, nexacro.Mojito);
+
+var pMojito = nexacro.Mojito.prototype;
 // ============================================================================
 
 // 랜덤 문자열 가져오기
-pMojito.getRandomString = function(num=8) {
+pMojito.getStringRandom = function(length=8) {
   const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   let result = '';
   const charactersLength = characters.length;
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
 
@@ -28,5 +30,6 @@ pMojito.getRandomString = function(num=8) {
 pMojito = null; delete pMojito;
 
 // ============================================================================
-$m=mojito=nexacro.Mojito=new nexacro._Mojito();
+
+$m=mojito=nexacro.mojito=new nexacro.Mojito();
 //$m.getRandomString();
