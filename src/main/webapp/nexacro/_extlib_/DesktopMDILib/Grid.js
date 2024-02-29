219e01fac90a71ec571a4f0ca37823bf0b041cf9
A86805 <A86805@LCSDEV1A2144.corp.lgcns.com>
2024-02-29 13:07:38 +0900

    2024.02.14 Grid

--- src/main/webapp/nexacro/_extlib_/DesktopMDILib/Grid.js
+++ src/main/webapp/nexacro/_extlib_/DesktopMDILib/Grid.js
@@ -1,9 +1,9 @@
 /**
 *  PcMdi 프로젝트 Library
-*  @FileName 	Grid.js 
+*  @FileName 	Grid.js
 *  @Creator 	TOBESOFT
 *  @CreateDate 	2023/10/30
-*  @Desction   
+*  @Desction
 ************** 소스 수정 이력 ***********************************************
 *  date          		Modifier                Description
 *******************************************************************************
@@ -12,10 +12,10 @@
 */
 
 var pForm = nexacro.Form.prototype;
-// 
+//
 
 pForm.GridConfig = {
-	"defaultmenulist" : ["sort"],	// default 메뉴	
+	"defaultmenulist" : ["sort"],	// default 메뉴
 	"sort" : 	{
 					sortTogleCancel		: true ,	// 헤더 클릭시 정렬 false= 오름/내림 true= 오름/내림/없음
 					ascText  			: "▲"  ,	// ascending text
@@ -26,10 +26,10 @@ pForm.GridConfig = {
 
 /**
 * @class  그리드 설정 내부함수<br>
-	   그리드에 유저프로퍼티를 Array형태로 반환한다.
+	그리드에 유저프로퍼티를 Array형태로 반환한다.
 * @param  {Object}objGrid	- 대상그리드
 * @return {Array} user property
-* @example this._getGridUserProperty(this.grdMain);	
+* @example this._getGridUserProperty(this.grdMain);
 */
 pForm._getGridUserProperty = function (objGrid)
 {
@@ -98,10 +98,10 @@ pForm.gfnGrid_onheadclick = function(objGrid, e)
 * @class 정렬가능여부리턴
 * @param {Object} grid - 대상그리드
 * @param {Number} headCellIndex - 대상셀INDEX
-* @param {Boolean} multiple - 멀티소트여부 
-* @param {Number} sortStatus - 소트상태  
+* @param {Boolean} multiple - 멀티소트여부
+* @param {Number} sortStatus - 소트상태
 * @return{Boolean} sort 가능/불가능 여부
-* @example this._gfnGridSetSortStatus(obj, e.cell, multiple);	
+* @example this._gfnGridSetSortStatus(obj, e.cell, multiple);
 */
 pForm._gfnGridSetSortStatus = function(grid, headCellIndex, isMultiple, sortStatus, bodyCellIndex)
 {
@@ -141,20 +141,20 @@ pForm._gfnGridSetSortStatus = function(grid, headCellIndex, isMultiple, sortStat
 		var headText = grid.getCellText(-1, headCellIndex);
 		
 		// executeSort에서 정렬 표시를 위해 cell index 가 필요한데
-		// cell moving 될 경우 index는 변하므로 cell object 를 참조하여 값을 얻어온다. 		
+		// cell moving 될 경우 index는 변하므로 cell object 를 참조하여 값을 얻어온다.
 		var refCell = this._gfnGridGetGridCellObject(grid, "head", headCellIndex);
 		sortInfo = sortInfos[columnName] = { status: 0, text: headText, refCell: refCell};
 	}
 	// set sort status
-	if ( isMultiple ) {		
+	if ( isMultiple ) {
 		status = sortInfo.status;
 		if ( sortStatus == -1 ) {
 			if ( status == 0 ) {
 				sortInfo.status = 1;
-			} 
+			}
 			else if ( status == 1 ) {
 				sortInfo.status = 2;
-			} 
+			}
 			else if ( status == 2 ) {
 				sortInfo.status = ( this.GridConfig.sort.sortTogleCancel ? 0 : 1);
 			}
@@ -173,10 +173,10 @@ pForm._gfnGridSetSortStatus = function(grid, headCellIndex, isMultiple, sortStat
 					if ( sortStatus == -1 ) {
 						if ( status == 0 ) {
 							sortInfo.status = 1;
-						} 
+						}
 						else if ( status == 1 ) {
 							sortInfo.status = 2;
-						} 
+						}
 						else if ( status == 2) {
 							sortInfo.status = ( this.GridConfig.sort.sortTogleCancel ? 0 : 1);
 						}
@@ -205,22 +205,22 @@ pForm._gfnGridSetSortStatus = function(grid, headCellIndex, isMultiple, sortStat
 			hasItem = true;
 			break;
 		}
-	}	
+	}
 	if (!hasItem) {
 		sortItems.push(columnName);
 	}
 	return true;
-}; 
+};
 
 /**
 * @class 소트를 실행한다
 * @param {Object}  grid 대상 Grid Component
 * @return{String}  N/A
-* @example this._gfnGridExecuteSort(obj);	
-*/  
-pForm._gfnGridExecuteSort = function(grid) 
+* @example this._gfnGridExecuteSort(obj);
+*/
+pForm._gfnGridExecuteSort = function(grid)
 {
-	var sortInfo, 
+	var sortInfo,
 		sortItem,
 		sortInfos = grid.sortInfos,
 		sortItems = grid.sortItems,
@@ -299,7 +299,7 @@ pForm._gfnGridExecuteSort = function(grid)
 		
 		if ( groupKeyString.length > 0 ) {
 			ds.set_keystring(groupKeyString + "," +  sortKeyString);
-		} 
+		}
 		else {
 			ds.set_keystring(sortKeyString);
 		}
@@ -316,7 +316,7 @@ pForm._gfnGridExecuteSort = function(grid)
 	for (var p in sortInfos) {
 		if ( sortInfos.hasOwnProperty(p) )
 		{
-			sortInfo = sortInfos[p];			
+			sortInfo = sortInfos[p];
 			cell = sortInfo.refCell;
 			if ( cell )
 			{
@@ -338,7 +338,7 @@ pForm._gfnGridExecuteSort = function(grid)
 * @class Grid에 기능 추가
 * @param {Object} objGrid	- 대상그리드
 * @return N/A
-* @example this.gfnSetGrid(this.grdMain);	
+* @example this.gfnSetGrid(this.grdMain);
 */
 pForm.gfnSetGrid = function(objGrid)
 {
@@ -354,14 +354,14 @@ pForm.gfnSetGrid = function(objGrid)
 	if(arrProp.indexOf("sort")>=0) objGrid.uFun_sort = true;
 	
 	objGrid.addEventHandler("onheadclick", this.gfnGrid_onheadclick, this);
-};	
+};
 
 /**
 * @class Grid에 Sort 취소
 * @param {Object} objGrid	- 대상그리드
 * @param {Number} nCell - 컬럼인덱스
 * @return N/A
-* @example this.gfnClearSortMark(this.grdMain, 0);	
+* @example this.gfnClearSortMark(this.grdMain, 0);
 */
 pForm.gfnClearSortMark = function (objGrid, nCell) {
 	var nColCnt = objGrid.getCellCount("head");
@@ -369,7 +369,7 @@ pForm.gfnClearSortMark = function (objGrid, nCell) {
 	
 	if(nCell === null || nCell === undefined) this._issortmark = false;
 
-	for (var ii=0; ii < nColCnt; ii++) 
+	for (var ii=0; ii < nColCnt; ii++)
 	{
 		if (objGrid.getCellProperty("head", ii, "displaytype") == "checkboxcontrol" ||
 			objGrid.getCellProperty("head", ii, "edittype") == "checkbox") continue;
@@ -387,9 +387,9 @@ pForm.gfnClearSortMark = function (objGrid, nCell) {
 * @param {Object} objGrid - 대상그리드
 * @param {Evnet}  e	   - 헤드클릭이벤트
 * @return  N/A
-* @example objGrid.gfnGridSelectAllCheckbox(obj, e);
+* @example objGrid.gfnIsGridCheckboxCell(obj, e);
 */
-pForm.gfnGridSelectAllCheckbox = function(objGrid, e)
+pForm.gfnIsGridCheckboxCell = function(objGrid, e)
 {
 	if (objGrid.readonly == true) return true;
 	var nCell = e.cell;
@@ -410,9 +410,9 @@ pForm.gfnGridSelectAllCheckbox = function(objGrid, e)
 		strVal = "0";
 	}
 	objGrid.setCellProperty( "head", nCell, "text", strVal);
-	dsObj.set_enableevent(false); 
+	dsObj.set_enableevent(false);
 	for ( var r = 0 ; r < dsObj.getRowCount() ; r++ ) {
-		dsObj.setColumn(r, strChkCol, strVal);  
+		dsObj.setColumn(r, strChkCol, strVal);
 	}
 	/*
 	if(!dsObj._ischeckevent) {
@@ -427,7 +427,7 @@ pForm.gfnGridSelectAllCheckbox = function(objGrid, e)
 								this);
 	}
 	*/
-	dsObj.set_enableevent(true);	
+	dsObj.set_enableevent(true);
 	dsObj = null;
 	
 	return true;
@@ -438,9 +438,9 @@ pForm.gfnGridSelectAllCheckbox = function(objGrid, e)
 * @param {Object}  objGrid - 대상 Grid Component
 * @param {Number}  nIndex - grid cell index
 * @return{String} column id
-* @example this.gfnGetBindColName(obj, e.cell);	
-*/  
-pForm.gfnGetBindColName = function(objGrid, nIndex) 
+* @example this.gfnGetBindColName(obj, e.cell);
+*/
+pForm.gfnGetBindColName = function(objGrid, nIndex)
 {
 	var text = "";
 	var columnid = "";
@@ -448,18 +448,18 @@ pForm.gfnGetBindColName = function(objGrid, nIndex)
 	if ( subCell > 0 )
 	{
 		text = objGrid.getSubCellProperty("body", nIndex, 0, "text");
-	} else 
+	} else
 	{
 		text = objGrid.getCellProperty("body", nIndex, "text");
 	}
 	
 	if (!this.gfnIsNull(text) )
 	{
-		if ( text.search(/^BIND\(/) > -1 ) 
-		{	
+		if ( text.search(/^BIND\(/) > -1 )
+		{
 			columnid = text.replace(/^BIND\(/, "");
 			columnid = columnid.substr(0, columnid.length-1);
-		} else if ( text.search(/^bind:/) > -1 ) 
+		} else if ( text.search(/^bind:/) > -1 )
 		{
 			columnid = text.replace(/^bind:/, "");
 		}
@@ -474,17 +474,17 @@ pForm.gfnGetBindColName = function(objGrid, nIndex)
 * @param {Number} bUseColspan - Colsapn 사용여부
 * @return{Number}  body cell index
 * @example this._gfnGridGetBodyCellIndex(this.grid, 0);
-*/ 
-pForm._gfnGridGetBodyCellIndex = function(objGrid, nHeadCellIndex, bUseColspan) 
+*/
+pForm._gfnGridGetBodyCellIndex = function(objGrid, nHeadCellIndex, bUseColspan)
 {
 	if (this.gfnIsNull(bUseColspan)) bUseColspan=false;
 	
 	// Max Head Row Index
 	var nMaxHeadRow = 0;
-	for (var i=0, len=objGrid.getCellCount("head"); i<len; i++) 
+	for (var i=0, len=objGrid.getCellCount("head"); i<len; i++)
 	{
 		var row = objGrid.getCellProperty("head", i, "row");
-		if (nMaxHeadRow < row) 
+		if (nMaxHeadRow < row)
 		{
 			nMaxHeadRow = row;
 		}
@@ -492,76 +492,76 @@ pForm._gfnGridGetBodyCellIndex = function(objGrid, nHeadCellIndex, bUseColspan)
 	
 	// Max Body Row Index
 	var nMaxBodyRow = 0;
-	for (var i=0, len=objGrid.getCellCount("body"); i<len; i++) 
+	for (var i=0, len=objGrid.getCellCount("body"); i<len; i++)
 	{
 		var row = objGrid.getCellProperty("body", i, "row");
-		if (nMaxBodyRow < row) 
+		if (nMaxBodyRow < row)
 		{
 			nMaxBodyRow = row;
 		}
 	}
 	
-	if (nMaxHeadRow == 0 && nMaxBodyRow == 0) 
+	if (nMaxHeadRow == 0 && nMaxBodyRow == 0)
 	{
 		bUseColspan = true;
 	}
 	
 	// Body Row 가 1개 이상일 경우
 	// Head의 row 가 Body의 row 보다 클 경우 차이 row 를 뺀 것을 대상으로 찾고
-	// Body의 row 가 Head의 row 보다 크거나 같을 경우 row index가 같은 대상을 찾는다.			
+	// Body의 row 가 Head의 row 보다 크거나 같을 경우 row index가 같은 대상을 찾는다.
 	var nCellIndex = -1;
 	var sRow = -1;
 	var nRow = parseInt(objGrid.getCellProperty("head", nHeadCellIndex, "row"));
 	var nCol = parseInt(objGrid.getCellProperty("head", nHeadCellIndex, "col"));
-	var nColspan = parseInt(objGrid.getCellProperty("head", nHeadCellIndex, "colspan"));				
+	var nColspan = parseInt(objGrid.getCellProperty("head", nHeadCellIndex, "colspan"));
 	
-	if (nMaxHeadRow > nMaxBodyRow) 
+	if (nMaxHeadRow > nMaxBodyRow)
 	{
 		sRow = nRow - (nMaxHeadRow - nMaxBodyRow);
 		sRow = (sRow < 0 ? 0 : sRow);
-	} else 
+	} else
 	{
 		sRow = nRow;
 	}
 	
 	var cRow, cCol, cColspan, cRowspan;
 	
-	for (var i=0, len=objGrid.getCellCount("body"); i<len; i++) 
+	for (var i=0, len=objGrid.getCellCount("body"); i<len; i++)
 	{
 		cRow = parseInt(objGrid.getCellProperty("body", i, "row"));
-		cCol = parseInt(objGrid.getCellProperty("body", i, "col"));	
-		cColspan = parseInt(objGrid.getCellProperty("body", i, "colspan"));					
+		cCol = parseInt(objGrid.getCellProperty("body", i, "col"));
+		cColspan = parseInt(objGrid.getCellProperty("body", i, "colspan"));
 		cRowspan = parseInt(objGrid.getCellProperty("body", i, "rowspan"));
-		if( cRowspan > 1 ) 
+		if( cRowspan > 1 )
 		{
-			if (bUseColspan) 
+			if (bUseColspan)
 			{
-				if (sRow >= cRow && nCol <= cCol && cCol < (nCol + nColspan)) 
-				{		
+				if (sRow >= cRow && nCol <= cCol && cCol < (nCol + nColspan))
+				{
 					nCellIndex = i;
 					break;
-				}		
-			} else 
+				}
+			} else
 			{
-				if (sRow >= cRow && nCol == cCol && nColspan == cColspan) 
-				{		
+				if (sRow >= cRow && nCol == cCol && nColspan == cColspan)
+				{
 					nCellIndex = i;
 					break;
 				}
 			}
-		} else 
-		{	
-			if (bUseColspan) 
+		} else
+		{
+			if (bUseColspan)
 			{
-				if (sRow == cRow && nCol <= cCol && cCol < (nCol + nColspan)) 
-				{		
+				if (sRow == cRow && nCol <= cCol && cCol < (nCol + nColspan))
+				{
 					nCellIndex = i;
 					break;
-				}		
-			} else 
+				}
+			} else
 			{
-				if (sRow == cRow && nCol == cCol && nColspan == cColspan) 
-				{		
+				if (sRow == cRow && nCol == cCol && nColspan == cColspan)
+				{
 					nCellIndex = i;
 					break;
 				}
@@ -585,15 +585,15 @@ pForm._gfnGridGetGridCellObject = function(objGrid, sBand, nIndex)
 	var objCell;
 	
 	var format = objGrid._curFormat;
-	if (format) 
+	if (format)
 	{
-		if (sBand == "head") 
+		if (sBand == "head")
 		{
 			objCell = format._headcells[nIndex];
-		} else if (sBand == "body") 
+		} else if (sBand == "body")
 		{
 			objCell = format._bodycells[nIndex];
-		} else if (sBand == "summ" || sBand == "summary") 
+		} else if (sBand == "summ" || sBand == "summary")
 		{
 			objCell = format._summcells[nIndex];
 		}
