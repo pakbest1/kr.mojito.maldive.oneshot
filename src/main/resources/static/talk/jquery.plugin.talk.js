a2c693fa32fe7b2a1c46c2c5a5790971df201fba
kissghost <kissghost@NB-A58Kh>
2024-04-27 12:24:03 +0900

    [2024.04.27] #01

--- src/main/resources/static/talk/jquery.plugin.talk.js
+++ src/main/resources/static/talk/jquery.plugin.talk.js
@@ -1,65 +1,66 @@
 (function($) {
-	let plugname = 'talk', $plug = $[plugname] = function(config) {
-		$plug.config = config;
+	let plugname = 'talk', $p = $[plugname] = function(config) {
+		$p.config = config;
 
-		$plug.send = function(s){
-			$plug.wsock.send(s);
+		$p.send = function(s){
+			$p.wsock.send(s);
 		};
 
-		$plug.init = function() {
-			// console.log('$plug.init()');
-			let config = $plug.config;
-			let wsurl  = location.origin.replace(/http:|https:/g, 'ws:')+ config.url;
+		//$p.init = function() {
+		// console.log('$p.init()');
+		//let config = $p.config;
+		let wsurl  = location.origin.replace(/http:|https:/g, 'ws:')+ config.url;
 
-			$plug.wsock = new WebSocket(wsurl);
-			Object.assign($plug.wsock, {
-				onopen   : (e)=>{
-					console.log('[onopen:'+ wsurl +']');
-				},
-				onclose  : (e)=>{
-					console.log('[onclose:]');
-				},
-				onerror  : (e)=>{
-					console.log('[onerror:]');
-				},
-				onmessage: (e)=>{
-					const {PING} = $plug.cmd, {PONG} = $plug.cmd;
-					let sock= $plug.wsock, data = e ? e.data : null;  //console.log('[onmessage:'+ e.data +']');
-					if (!data) { return; }
-					if (data.startsWith(PING)) {
-						let r = data.replace(new RegExp(PING, 'g'), PONG);  console.log('[>'+ r +'<]');
-						sock.send(r);
-						e.preventDefault();
-						return;
-					}
-
-					if (config.recive) { config.recive(e); }
-				},
-			});
-
-			if (config.form && config.send) {
-				$(config.form).on('submit', (e)=>{
-					config.send(e);
+		$p.wsock = new WebSocket(wsurl);
+		Object.assign($p.wsock, {
+			onopen   : (e)=>{
+				console.log('[onopen:'+ wsurl +']');
+			},
+			onclose  : (e)=>{
+				console.log('[onclose:]');
+			},
+			onerror  : (e)=>{
+				console.log('[onerror:]');
+			},
+			onmessage: (e)=>{
+				const {PING} = $p.cmd, {PONG} = $p.cmd;
+				let sock= $p.wsock, data = e ? e.data : null;  //console.log('[onmessage:'+ e.data +']');
+				if (!data) { return; }
+				if (data.startsWith(PING)) {
+					let r = data.replace(new RegExp(PING, 'g'), PONG);  console.log('[>'+ r +'<]');
+					sock.send(r);
 					e.preventDefault();
-					return false;
-				});
-			};
+					return;
+				}
 
-			if (config.init) {
-				config.init({
-					target: config.form,
-					parameter: config.parameter,
-				});
-			}
+				if (config.recive) { config.recive(e); }
+			},
+		});
 
-			return $plug;
-		}();
-		$plug.cmd = {
+		if (config.form && config.send) {
+			$(config.form).on('submit', (e)=>{
+				config.send(e);
+				e.preventDefault();
+				return false;
+			});
+		};
+
+		if (config.init) {
+			config.init({
+				target: config.form,
+				parameter: config.parameter,
+			});
+		}
+
+		//}();
+		$p.cmd = {
 			EMPTY: ''    ,
 			PING : 'ping',
 			PONG : 'pong',
 		};
-		Object.freeze($plug.cmd);
+		Object.freeze($p.cmd);
+		
+		return $p;
 	};
 
 })(jQuery);
