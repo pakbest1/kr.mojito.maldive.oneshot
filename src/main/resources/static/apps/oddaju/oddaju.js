1c456306081fa7d8635e6299789a2e98b2bea84f
sunggeun1.park <sunggeun1.park@GNRHQ10-NE1027A.LGE.NET>
2024-05-27 15:27:44 +0900

    2024.05.27 #02

--- src/main/resources/static/apps/oddaju/oddaju.js
+++ src/main/resources/static/apps/oddaju/oddaju.js
@@ -1,20 +1,35 @@
+const
 
-
-const _getObject = (entries) => {
+_getObject = (entries) => {
 	const result = {};
 	if (!entries) { return result; }
 	for (const [key, value] of entries) {
 		result[key] = value;
 	}
 	return result;
-};
-const getObject = (s) => {
+},
+getObject = (s) => {
 	s = s.startsWith('#') ? s.substring(1) : s;
 	return _getObject(new URLSearchParams(s).entries());
-};
-const getObjectFromQueryString = () => {
+},
+getObjectFromQueryString = () => {
 	return getObject(location.search);
-};
-const getObjectFromHash = () => {
+},
+getObjectFromHash = () => {
 	return getObject(location.hash);
 };
+getDDLInfo = (sql) => {
+	if (!sql) { return null; }
+	let mch = /\s+(table|view|package|procedure|function|trigger)\s+(\w+)*/i.exec(sql), r = {};
+
+	if (mch && mch[1]) { r.type = mch[1]; }
+	if (mch && mch[2]) { r.name = mch[2]; }
+
+	return Object.keys(r).length === 0 ? null : r;
+},
+
+
+
+
+
+null;
