82e56549ec1477ed1bb45162d2b912154a8f2103
sunggeun1.park <sunggeun1.park@GNRHQ10-NE1027A.LGE.NET>
2024-05-30 17:11:12 +0900

    2024.05.30 #02

--- src/main/java/kr/mojito/maldive/oneshot/app/utils/HttpConsts.java
+++ src/main/java/kr/mojito/maldive/oneshot/app/utils/HttpConsts.java
@@ -15,12 +15,20 @@ public interface HttpConsts {
 	}
 	
 	public interface MediaTypes {
-		@NonNull final MediaType MEDIATYPE_TEXT            = MediaType.parse("text/plain"                       );
-		@NonNull final MediaType MEDIATYPE_HTML            = MediaType.parse("text/html"                        );
-		@NonNull final MediaType MEDIATYPE_XML             = MediaType.parse("text/xml"                         );
-		@NonNull final MediaType MEDIATYPE_JSON            = MediaType.parse("application/json"                 );
-		@NonNull final MediaType MEDIATYPE_FORM_DATA       = MediaType.parse("multipart/form-data"              );
-		@NonNull final MediaType MEDIATYPE_FORM_URLENCODED = MediaType.parse("application/x-www-form-urlencoded");
-		@NonNull final MediaType MEDIATYPE_RAW_DATA        = MediaType.parse("application/octet-stream"         );
+		@NonNull final MediaType MEDIATYPE_XML                    = MediaType.parse("text/xml"                         );
+		@NonNull final MediaType MEDIATYPE_TEXT                   = MediaType.parse("text/plain"                       );
+		@NonNull final MediaType MEDIATYPE_HTML                   = MediaType.parse("text/html"                        );
+		@NonNull final MediaType MEDIATYPE_JSON                   = MediaType.parse("application/json"                 );
+		@NonNull final MediaType MEDIATYPE_FORM_DATA              = MediaType.parse("multipart/form-data"              );
+		@NonNull final MediaType MEDIATYPE_FORM_URLENCODED        = MediaType.parse("application/x-www-form-urlencoded");
+		@NonNull final MediaType MEDIATYPE_RAW_DATA               = MediaType.parse("application/octet-stream"         );
+		
+		@NonNull final String    MEDIATYPE_XML_STRING             = MediaType.parse("text/xml"                         ).toString();
+		@NonNull final String    MEDIATYPE_TEXT_STRING            = MediaType.parse("text/plain"                       ).toString();
+		@NonNull final String    MEDIATYPE_HTML_STRING            = MediaType.parse("text/html"                        ).toString();
+		@NonNull final String    MEDIATYPE_JSON_STRING            = MediaType.parse("application/json"                 ).toString();
+		@NonNull final String    MEDIATYPE_FORM_DATA_STRING       = MediaType.parse("multipart/form-data"              ).toString();
+		@NonNull final String    MEDIATYPE_FORM_URLENCODED_STRING = MediaType.parse("application/x-www-form-urlencoded").toString();
+		@NonNull final String    MEDIATYPE_RAW_DATA_STRING        = MediaType.parse("application/octet-stream"         ).toString();
 	}
 }
--- src/main/java/kr/mojito/maldive/oneshot/app/utils/HttpUtil.java
+++ src/main/java/kr/mojito/maldive/oneshot/app/utils/HttpUtil.java
@@ -1,10 +1,18 @@
 package kr.mojito.maldive.oneshot.app.utils;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import okhttp3.CertificatePinner;
+import okhttp3.FormBody;
+import okhttp3.FormBody.Builder;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -15,10 +23,12 @@ import okhttp3.Response;
 public class HttpUtil {
 	private HttpUtil() {}
 	
-	private static Object _request(String surl, String method, String contenttype, Object body) {
-		if (surl        == null) { return null; }
-		if (method      == null) { method      = HttpConsts.Methods.GET              .toString(); }
-		if (contenttype == null) { contenttype = HttpConsts.MediaTypes.MEDIATYPE_HTML.toString(); }
+	@SuppressWarnings("unchecked")
+	private static Object _request(String surl, String method, String mediatype, Object body) {
+		if (surl      == null) { return null; }
+		
+		if (method    == null) { method    = HttpConsts.Methods.GET                     ; }
+		if (mediatype == null) { mediatype = HttpConsts.MediaTypes.MEDIATYPE_HTML_STRING; }
 		
 		String s = null;
 		URL url;
@@ -41,7 +51,33 @@ public class HttpUtil {
 				//okhttp3.MediaType.get(s)
 				//okhttp3.MediaType.get("html");
 				//MediaTypes.MEDIATYPE_HTML
-				requestBuilder.post(RequestBody.create(okhttp3.MediaType.parse("application/json"), ((String) body)));
+
+				if (HttpConsts.MediaTypes.MEDIATYPE_JSON_STRING.equals(mediatype)) {
+					String j = null;
+					try {
+						j = body instanceof String ? (String) body : (new ObjectMapper()).writeValueAsString(body);
+					} catch (Exception e) {
+						j = null;
+					}
+					
+					requestBuilder.post(RequestBody.create(MediaType.parse(mediatype), j));
+				} else if (HttpConsts.MediaTypes.MEDIATYPE_FORM_DATA_STRING.equals(mediatype)) {
+					FormBody formBody        = null;
+					Builder  formBodyBuilder = new FormBody.Builder();
+					if (body instanceof Map) {
+						Map<String, ?> map = (Map<String, ?>) body;
+						
+						for (Entry<String, ?> entry : map.entrySet()) {
+							String key = entry.getKey(), value = entry.getValue() == null ? null : entry.getValue().toString();
+							formBodyBuilder.add(key, value);
+						}
+						
+						formBody = formBodyBuilder.build();
+					}
+					requestBuilder.post(formBody);
+				}
+				
+				
 			}
 			
 			Response response = client.newCall(requestBuilder.build()).execute();
@@ -91,14 +127,23 @@ public class HttpUtil {
 	
 	
 	public static void main(String[] args) {
+		String s;
+		
 		okhttp3.MediaType mediaType = okhttp3.MediaType.get("text/html;charset=utf-8");
 		log(mediaType.toString());
 		
-		String s;
-		s = (String) HttpUtil.requestGet("https://publicobject.com/robots.txt");
-		log(s, "HttpUtil.requestGet");
+//		// case : get
+//		s = (String) HttpUtil.requestGet("https://publicobject.com/robots.txt");
+//		log(s, "HttpUtil.requestGet");
 		
-		s = (String) HttpUtil.request("https://publicobject.com/robots.txt", HttpConsts.Methods.POST, HttpConsts.MediaTypes.MEDIATYPE_RAW_DATA.toString());
+		
+		// case : post
+		Map<String, Object> mbody = new HashMap<String, Object>() {{
+			put("param1", "1");
+			put("param2", "2");
+			put("param3", "3");
+		}};
+		s = (String) HttpUtil.request("https://publicobject.com/robots.txt", HttpConsts.Methods.POST, HttpConsts.MediaTypes.MEDIATYPE_RAW_DATA.toString(), mbody);
 		log(s, "HttpUtil.request");
 		
 	}
